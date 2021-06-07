dcc81f2707b1735a5975b664948329c28b0963ee
neezystudio <victorneez6@gmail.com>
2021-06-07 14:26:27 +0300

    ive updated the firestorw file  checkbelow

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD-xwJe109FDqICiVVBBi2AGd63NdmEgjQ",
+    authDomain: "shikilia-e-commerce.firebaseapp.com",
+    projectId: "shikilia-e-commerce",
+    storageBucket: "shikilia-e-commerce.appspot.com",
+    messagingSenderId: "792280871913",
+    appId: "1:792280871913:web:672d8a3fbc96edfbf95178",
+    measurementId: "G-RLTN8KEDSM"
+};
